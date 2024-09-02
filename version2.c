4800d86fd1a6aee265f85cab32bfedf4d9d8af75
NAQUENO <yureikisin@gmail.com>
2024-09-02 01:04:41 +0000

    Version2 implmementada

--- version2.c
+++ version2.c
@@ -2,7 +2,8 @@
 
 int main() {
     int num1, num2;
-    int suma, resta;
+    int suma, resta, multiplicacion;
+    float division;
 
     printf("Ingrese el primer número: ");
     scanf("%d", &num1);
@@ -11,9 +12,14 @@ int main() {
 
     suma = num1 + num2;
     resta = num1 - num2;
+    multiplicacion = num1 * num2;
+    division = (float)num1 / num2; 
 
+    
     printf("La suma de %d y %d es: %d\n", num1, num2, suma);
     printf("La resta de %d y %d es: %d\n", num1, num2, resta);
+    printf("La multiplicación de %d y %d es: %d\n", num1, num2, multiplicacion);
+    printf("La división de %d entre %d es: %.2f\n", num1, num2, division);
 
     return 0;
 }
