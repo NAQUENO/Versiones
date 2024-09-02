2c5ec44a0b8dc6f7bdff17d3b04bbcb7e073b4c4
NAQUENO <yureikisin@gmail.com>
2024-09-02 01:13:31 +0000

    Version 3 implementada

---
+++ version3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    int suma, resta, multiplicacion;
+    float division;
+
+    printf("Ingrese el primer número: ");
+    scanf("%d", &num1);
+    printf("Ingrese el segundo número: ");
+    scanf("%d", &num2);
+
+    suma = num1 + num2;
+    resta = num1 - num2;
+    multiplicacion = num1 * num2;
+    division = (float)num1 / num2;
+
+    printf("La suma de %d y %d es: %d\n", num1, num2, suma);
+    printf("La resta de %d y %d es: %d\n", num1, num2, resta);
+    printf("La multiplicación de %d y %d es: %d\n", num1, num2, multiplicacion);
+
+    if (num2 != 0) {    
+        printf("La división de %d entre %d es: %.2f\n", num1, num2, division);
+    } else {
+        printf("Division Error: No se puede dividir entre 0.\n");
+    }
+
+    return 0;
+}
